32f9be674b08c8d3fe8a69f51d90dd30b3687103
Kaige Liu <kaigel@andrew.cmu.edu>
2018-01-20 09:07:24 -0500

    Update socket pi

--- pi/main.py
+++ pi/main.py
@@ -3,10 +3,10 @@ import socket, select
 from time import gmtime, strftime
 from random import randint
 from recgnition import *
-#from picamera import PiCamera
+from picamera import PiCamera
 from time import sleep
 
-#camera = PiCamera()
+camera = PiCamera()
 imgPrefix = "/home/pi/Desktop/images/image_"
 curImageLoc = '/home/pi/Desktop/image.jpg'
 
@@ -16,20 +16,14 @@ def find(sock, name):
     interval = 0.1
     reportInterval = 10
     while True:
-        #camera.capture(curImageLoc)
+        camera.capture(curImageLoc)
         (x,y,w,h) = recgnition(imgPrefix+name,curImageLoc)
         sleep(interval)
         time += 1
         if (time % reportInterval == 0):
             sock.sendall("instr hahahahahaha" + "\n")
 
-
-
-
-
-
-
-HOST = '192.168.50.64'
+HOST = '192.168.50.45'
 PORT = 6666
 #setup sockets
 serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -45,26 +39,26 @@ while True:
     name = ""
     size = 0
     if data:
-        if data.startswith("DEF\n"):
+        if data.startswith("DEF"):
             tmp = txt.split()
             name = tmp[1]
             size = int(tmp[2])
             print 'got size'
             sock.sendall("GOTDEF\n")
-        elif data.startswith("FIND\n"):
+        elif data.startswith("FIND"):
             tmp = txt.split()
             name = tmp[1]
             sock.sendall("GOTFIND\n")
             find(sock,name)
         else :
-            #myfile = open(imgPrefix+name, 'wb')
-            #myfile.write(data)
+            myfile = open(imgPrefix+name, 'wb+')
+            myfile.write(data)
             data = sock.recv(40960000)
-            #if not data:
-            #    myfile.close()
-            #else:
-            #    myfile.write(data)
-            #    myfile.close()
+            if not data:
+                myfile.close()
+            else:
+                myfile.write(data)
+                myfile.close()
             print data
             sock.sendall("GOTIMAGE\n")
 
