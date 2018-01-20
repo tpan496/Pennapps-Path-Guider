635176ddaf050a85d1ae2fec601e6471e40ab9a4
Kaige Liu <kaigel@andrew.cmu.edu>
2018-01-19 20:35:08 -0500

    Camera update test

--- pi/test.py
+++ pi/test.py
@@ -6,7 +6,7 @@ camera = PiCamera()
 datapath = '/home/pi/Desktop/'
 i = 0
 while True:
-    camera.capture(datapath + 'image'+i+'.jpg')
+    camera.capture(datapath + 'image'+str(i)+'.jpg')
     sleep(1000)
 
 
