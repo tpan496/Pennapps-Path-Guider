ca1e0532c86957c574cfd6d40c6af6055e3d0bd5
tpan496 <tianleip@andrew.cmu.edu>
2018-01-20 10:54:31 -0500

    i do not know da way

--- path_guider_app/app/src/main/java/pantianlei/pathguider/MainActivity.java
+++ path_guider_app/app/src/main/java/pantianlei/pathguider/MainActivity.java
@@ -8,18 +8,16 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import io.socket.client.Socket;
-import io.socket.emitter.Emitter;
-
 public class MainActivity extends AppCompatActivity {
 
     private final static String TAG = "main";
 
     private Button connectButton;
-    private String addr;
-    private int port;
+    private Button sendButton;
     private TextView response;
-    private Socket mSocket;
+    private Client client;
+    private String ip;
+    private int port;
 
     private boolean isConnected = true;
 
@@ -29,82 +27,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         connectButton = (Button) findViewById(R.id.connectButton);
+        sendButton = (Button) findViewById(R.id.sendButton);
         response = (TextView) findViewById(R.id.connectText);
 
         final PathGuiderApplication app = (PathGuiderApplication) getApplication();
+        ip = "192.168.50.64";
+        port = 6666;
 
-        mSocket = app.getSocket();
+        client = new Client(ip, port);
+
         connectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mSocket.disconnect();
-                mSocket.connect();
+                client = new Client(ip, port);
+                client.connect();
             }
         });
 
-        mSocket.on("new_user", onConnect);
-        mSocket.on(Socket.EVENT_CONNECT, onConnect);
-        mSocket.on(Socket.EVENT_DISCONNECT,onDisconnect);
-        mSocket.on(Socket.EVENT_CONNECT_ERROR, onConnectError);
-        mSocket.on(Socket.EVENT_CONNECT_TIMEOUT, onConnectError);
-    }
-
-    private Emitter.Listener onConnect = new Emitter.Listener() {
-        @Override
-        public void call(Object... args) {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    if(!isConnected) {
-                        mSocket.emit("Device connected", "connected!");
-                        Toast.makeText(getApplicationContext(),
-                                "connected", Toast.LENGTH_LONG).show();
-                        Log.e(TAG, "connected");
-                        isConnected = true;
-                    }
-                }
-            });
-        }
-    };
-
-    private Emitter.Listener onDisconnect = new Emitter.Listener() {
-        @Override
-        public void call(Object... args) {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    Log.i(TAG, "diconnected");
-                    isConnected = false;
-                    Toast.makeText(getApplicationContext(),
-                            "disconnected", Toast.LENGTH_LONG).show();
-                }
-            });
-        }
-    };
-
-    private Emitter.Listener onConnectError = new Emitter.Listener() {
-        @Override
-        public void call(Object... args) {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    Log.e(TAG, "Error connecting");
-                    Toast.makeText(getApplicationContext(),
-                            "connection error", Toast.LENGTH_LONG).show();
-                }
-            });
-        }
-    };
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-
-        mSocket.disconnect();
-
-        mSocket.off(Socket.EVENT_CONNECT, onConnect);
-        mSocket.off(Socket.EVENT_DISCONNECT, onDisconnect);
-        mSocket.off(Socket.EVENT_CONNECT_ERROR, onConnectError);
-        mSocket.off(Socket.EVENT_CONNECT_TIMEOUT, onConnectError);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                client.sendDEF("wtf gg smida");
+            }
+        });
     }
 }
