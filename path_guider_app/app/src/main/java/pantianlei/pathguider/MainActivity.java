02afffb878bac1bbbe53246f92a621cc6e5e0ac2
tpan496 <tianleip@andrew.cmu.edu>
2018-01-19 21:42:48 -0500

    socket

--- path_guider_app/app/src/main/java/pantianlei/pathguider/MainActivity.java
+++ path_guider_app/app/src/main/java/pantianlei/pathguider/MainActivity.java
@@ -2,12 +2,109 @@ package pantianlei.pathguider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = "main";
+
+    private Button connectButton;
+    private String addr;
+    private int port;
+    private TextView response;
+    private Socket mSocket;
+
+    private boolean isConnected = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        connectButton = (Button) findViewById(R.id.connectButton);
+        response = (TextView) findViewById(R.id.connectText);
+
+        final PathGuiderApplication app = (PathGuiderApplication) getApplication();
+
+        mSocket = app.getSocket();
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSocket.disconnect();
+                mSocket.connect();
+            }
+        });
+
+        mSocket.on("new_user", onConnect);
+        mSocket.on(Socket.EVENT_CONNECT, onConnect);
+        mSocket.on(Socket.EVENT_DISCONNECT,onDisconnect);
+        mSocket.on(Socket.EVENT_CONNECT_ERROR, onConnectError);
+        mSocket.on(Socket.EVENT_CONNECT_TIMEOUT, onConnectError);
+    }
+
+    private Emitter.Listener onConnect = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(!isConnected) {
+                        mSocket.emit("Device connected", "connected!");
+                        Toast.makeText(getApplicationContext(),
+                                "connected", Toast.LENGTH_LONG).show();
+                        Log.e(TAG, "connected");
+                        isConnected = true;
+                    }
+                }
+            });
+        }
+    };
+
+    private Emitter.Listener onDisconnect = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.i(TAG, "diconnected");
+                    isConnected = false;
+                    Toast.makeText(getApplicationContext(),
+                            "disconnected", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    };
+
+    private Emitter.Listener onConnectError = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.e(TAG, "Error connecting");
+                    Toast.makeText(getApplicationContext(),
+                            "connection error", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    };
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mSocket.disconnect();
+
+        mSocket.off(Socket.EVENT_CONNECT, onConnect);
+        mSocket.off(Socket.EVENT_DISCONNECT, onDisconnect);
+        mSocket.off(Socket.EVENT_CONNECT_ERROR, onConnectError);
+        mSocket.off(Socket.EVENT_CONNECT_TIMEOUT, onConnectError);
     }
 }
